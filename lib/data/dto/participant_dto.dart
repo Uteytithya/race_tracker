6beb2e6c886108bb3109f57c57781cd15ddd76fd
Uteytithya <tithyautey@gmail.com>
2025-04-23 14:11:30 +0700

    RT-43 Finished DTO for participants

---
+++ lib/data/dto/participant_dto.dart
@@ -0,0 +1,25 @@
+import 'package:race_tracker/model/participant.dart';
+import 'package:race_tracker/utils/enum.dart';
+
+class ParticipantDTO {
+  static Map<String, dynamic> toJson(Participant model) {
+    return {'name': model.name, 'gender': model.gender, 'age': model.age};
+  }
+
+  static Gender genderFromString(String value) {
+    return Gender.values.firstWhere(
+      (e) => e.name.toLowerCase() == value.toLowerCase(),
+      orElse: () {
+        throw Exception("Invalid gender value: $value");
+      },
+    );
+  }
+
+  static Participant fromJson(Map<String, dynamic> json) {
+    return Participant(
+      name: json['name'],
+      gender: genderFromString(json['gender']),
+      age: json['age'],
+    );
+  }
+}
